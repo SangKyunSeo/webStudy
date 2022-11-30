2debefb603778f126a95616c7386462c02bd6b03
Sangkyun Seo <tkdrbs0802@naver.com>
2022-12-01 01:07:40 +0900

    feat: 회원가입 기능 구현

---
+++ src/main/java/com/spring/dto/MemberVO.java
@@ -0,0 +1,67 @@
+package com.spring.dto;
+
+public class MemberVO {
+    private String memberId;
+    private String memberPw;
+    private String memberName;
+    private int memberAge;
+    private String memberGender;
+    private String memberAddress;
+    private String memberPhone;
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getMemberPw() {
+		return memberPw;
+	}
+
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public int getMemberAge() {
+		return memberAge;
+	}
+
+	public void setMemberAge(int memberAge) {
+		this.memberAge = memberAge;
+	}
+
+	public String getMemberGender() {
+		return memberGender;
+	}
+
+	public void setMemberGender(String memberGender) {
+		this.memberGender = memberGender;
+	}
+
+	public String getMemberAddress() {
+		return memberAddress;
+	}
+
+	public void setMemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+
+	public String getMemberPhone() {
+		return memberPhone;
+	}
+
+	public void setMemberPhone(String memberPhone) {
+		this.memberPhone = memberPhone;
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" language="java" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	반갑습니다! 여기는 온라인 쇼핑몰 입니다!
+</h1>
+
+</body>
+</html>
